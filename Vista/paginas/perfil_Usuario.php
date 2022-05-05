e64387d3ee0dda301af7753f63ab2d668ab9cca6
marceldgr <100010339+marceldgr@users.noreply.github.com>
2022-05-05 01:50:05 -0500

    modificacion
    se arreglo algunos detalles

--- Vista/paginas/perfil_Usuario.php
+++ Vista/paginas/perfil_Usuario.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>perfil de usuario</title>
     <link rel="stylesheet" href="../css/style_usuario.css">
- </head>
+</head>
 
 <body>
     <header>
@@ -29,20 +29,22 @@
 
                     </ul>
                 </nav>
-                </div>
-                <i class="fa-solid fa-bars" id="icon_menu"></i>
-                      <div class="header_registro">
-                            <a href="../../Vista/Login.php">
-                             <div class="btn_logins">
-                          <input type="button" class="btn_login" value="Salir">
-                         </div>
-                              </a>
+            </div>
+            <i class="fa-solid fa-bars" id="icon_menu"></i>
+            <div class="header_registro">
+                <a href="../../Vista/Login.php">
+                    <div class="btn_logins">
+                        <input type="button" class="btn_login" value="Salir">
                     </div>
+                </a>
+            </div>
         </div>
     </header>
     <hr>
-    <div class="cuerpo">
-        <div class="articles_izq">
+    <div class="cuerpo1">
+        <input type="checkbox" class="toggle-check" id="toggle" hidden>
+        <label class="toggle" for="toggle">editar</label>
+        <div class="containerEditar">
             <div class="editar">
                 <form action="" class="EDITAR">
                     <h1>Editar</h1>
@@ -56,72 +58,58 @@
                 </form>
             </div>
         </div>
-        <div class="article_foto">
-            <div class="foto_perfil">
-                <h2>Foto de perfil</h2>
-                <div class="box_img_user">
-                    <img src="../img/persona/usurio.png" >
-                </div>
+    </div>
+    <!--segundo cuerpo -->
+    <div class="article_foto">
+        <div class="foto_perfil">
+            <h2>Foto de perfil</h2>
+            <div class="box_img_user">
+                <img src="../img/persona/usurio.png">
             </div>
-            <div class="article_datos_usuario">
-                <div class="infromacion_perfil">
-                    <form action="" class="Informacion">
-                        <h1>Datos del usuario</h1>
-                        <h5>
-                            Nombre Completo
-                        </h5>
-                        <input type="text" placeholder="Nombre completo" name="nombre_completo">
-                        <h5>
-                            Emial
-                        </h5>
-                        <input type="text" placeholder="correo Electronico" name="correo_electronico">
-                        <h5>
-                            Nombre de usuario
-                        </h5>
-                        <input type="text" placeholder="Usuario" name="usuario">
-                    </form>
+            <div class="infromacion_perfil">
+                <form action="" class="Informacion">
+                    <h1>Datos del usuario</h1>
+                    <h5>Nombre Completo</h5>
+                    <input type="text" placeholder="Nombre completo" name="nombre_completo">
+                    <h5>Emial</h5>
+                    <input type="text" placeholder="correo Electronico" name="correo_electronico">
+                    <h5>Nombre de usuario</h5>
+                    <input type="text" placeholder="Usuario" name="usuario">
+                </form>
+            </div>
+        </div>
+            <!--**************************************************************-->
+            <br>
+            <hr>
 
-                    <div class="perosna">
-                        <div class="box_img_per">
-                            <img src="../img/persona/per002.png">
-                        </div>
+        <div class="tarjeta">
+                <form action="" class="DatosTarjeta">
+                    <h1>Datos medio de pagos </h1>
+                    <h5>
+                        Numero de Tarjeta
+                    </h5>
+                    <input type="text" placeholder="Numero de Tarjeta" name="NumeroTarjeta">
+                    <h5>
+                        Email
+                    </h5>
+                    <input type="text" placeholder="correo Electronico" name="correo_electronico">
+                    <h5>
+                        Nombre de Titular
+                    </h5>
+                    <input type="text" placeholder="Titular" name="Titular">
+                    <h5>
+                        Direccion
+                    </h5>
+                    
+                    <button class="ediatr"> Editar</button>
+                    <button class="quitar">Quitar</button>
+                </form>
+                <div class="img_tarjeta">
+                    <div class="box_tarjeta">
+                        <img src="../img/logo_hack/credit-card.png">
                     </div>
-                    </div>
-                    <br>
-                    <hr>
-                    <div class="article_tarjeta_pagos">
-                        <div class="tarjeta">
-                            <form action="" class="DatosTarjeta">
-                                <h1>Datos medio de pagos </h1>
-                                <h5>
-                                    Numero de Tarjeta
-                                </h5>
-                                <input type="text" placeholder="Numero de Tarjeta" name="NumeroTarjeta">
-                                <h5>
-                                    Email
-                                </h5>
-                                <input type="text" placeholder="correo Electronico" name="correo_electronico">
-                                <h5>
-                                    Nombre de Titular
-                                </h5>
-                                <input type="text" placeholder="Titular" name="Titular">
-                                <h5>
-                                    Direccion
-                                </h5>
-                                <input type="text" placeholder="Direccion" name="Direccion">
-
-                                <button class="ediatr"> Editar</button>
-                                <button class="quitar">Quitar</button>
-                            </form>
-                            <div class="img_tarjeta">
-                                <div class="box_tarjeta">
-                                    <img src="../img/logo_hack/credit-card.png">
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
+                </div>
+        </div>
+    </div>
 </body>
-
 </html>
