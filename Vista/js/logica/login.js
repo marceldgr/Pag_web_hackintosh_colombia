c02900538910370ebfa025b145070da8580596ec
marceldgr <100010339+marceldgr@users.noreply.github.com>
2022-05-13 23:38:21 -0500

    Update login.js

---
+++ Vista/js/logica/login.js
@@ -0,0 +1,8 @@
+function onSignIn(googleUser){
+    let profile=googleUser.getBasicProfile()
+    localStorage.setItem('id',profile.getId())
+    localStorage.setItem('name',profile.getName())
+    localStorage.setItem('email',profile.getEmail())
+    localStorage.setItem('image',profile.getImageUrl())
+    window.location.replace(" ")
+}
