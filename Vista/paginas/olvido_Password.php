8371e85d53fa1905e06f0dad976029d75701ac48
marceldgr <100010339+marceldgr@users.noreply.github.com>
2022-05-24 00:55:01 -0500

    updata

---
+++ Vista/paginas/olvido_Password.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class ="text center">
+    <div class="form-signin">
+        <h1 class="h3 mb-3 font-weight-normal">¿olvido Password?</h1>
+        <input name="Email" type="text" id="Email" class="form-control" placeholder="">
+
+        <div class="row">
+            <button id="btn_enviar" class="btn btn-lg btn-primary btn-block">
+                <h8>ENVIAR</h8>
+            </button>
+            <div class="col-ms">
+                <button id="btn_Cancelar" class="btn btn-lg btn-primary btn">
+                    <h8>CANCELAR</h8>
+                </button>
+            </div>
+        </div>
+
+    <script src="../js/librerias/jquery-3.3.1.min.js"></script>
+    <script src="../js/librerias/bootstrap.min.js"></script>
+    <script src="../js/librerias/bootstrap.bundle.min.js"></script>
+    <script src="../js/librerias/sweetalert2.js"></script>
+    <script src="../js/logica/olvidocontrasena.js"></script>
+</body>
+</html>
