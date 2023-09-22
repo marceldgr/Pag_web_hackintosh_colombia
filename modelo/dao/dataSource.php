05cb4ef26416c25f53b6ccd479d52d645ba35128
hackintosh colombia <100010339+marceldgr@users.noreply.github.com>
2023-09-21 20:41:51 -0500

    Update dataSource.php
    conexión de base datos

--- modelo/dao/dataSource.php
+++ modelo/dao/dataSource.php
@@ -3,9 +3,12 @@ class DataSource{
      private $cadena_Conexion;
      private $conexion;
 //base de dastos 
+
+     
      public function __construct(){
          try{
-             $this->cadena_Conexion="mysql:host=localhost;dbname=id18809376_hackintosh_colombia;charset=utf8";
+             $this->cadena_Conexion=sqlsrv:server = tcp:vacasvaqueras.database.windows.net,1433; Database = dev_DB_Vacas_Vaqueras", "root1", "{Admin123}");
+             //revisar esta conexion
              $this->conexion=new PDO($this->cadena_Conexion,"id18809376_hackintosh_colombia_","IL0t@0B#/FXD}1XX");
          }catch (PDOException $ex) {
              echo $ex->getMessage();
